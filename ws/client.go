7f2f5f11433c58793a4c7c8c0a602d621de04a90
Jakub Pacewicz <115111601+pecet3@users.noreply.github.com>
2024-01-23 16:03:36 +0000

    copied prev webscoekt project to modify it

---
+++ ws/client.go
@@ -0,0 +1,58 @@
+package ws
+
+import (
+	"log"
+	"sync"
+
+	"github.com/gorilla/websocket"
+	"github.com/pecet3/czatex/utils"
+)
+
+type client struct {
+	name    string
+	conn    *websocket.Conn
+	receive chan []byte
+	room    *room
+}
+
+func (c *client) read() {
+	defer c.conn.Close()
+
+	for {
+		_, msg, err := c.conn.ReadMessage()
+		if err != nil {
+			return
+		}
+
+		c.room.forward <- msg
+	}
+}
+
+func (c *client) write() {
+	defer c.conn.Close()
+
+	for msg := range c.receive {
+
+		result, err := utils.DecodeMessage(msg)
+
+		var wg sync.WaitGroup
+		namesChan := make(chan []string)
+
+		wg.Add(1)
+
+		go createNamesArr(c.room.clients, &wg, namesChan)
+
+		namesArr := <-namesChan
+		close(namesChan)
+
+		message, err := utils.MarshalJsonMessage(result.Name, result.Message, namesArr)
+
+		log.Println("new message in room: ", c.room.name)
+
+		err = c.conn.WriteMessage(websocket.TextMessage, message)
+
+		if err != nil {
+			return
+		}
+	}
+}
